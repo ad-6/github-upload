4a9fb7d0414567e2db4e4bbb65e22911cc99982f
ad-6 <80580304+ad-6@users.noreply.github.com>
2021-04-10 09:11:09 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+/**
+ * @file helloworld.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("a random number");
+    scanf("%d", &n);
+    printf("you seleceted %d \n", n);
+   // printf("Hello world"); 
+
+}
